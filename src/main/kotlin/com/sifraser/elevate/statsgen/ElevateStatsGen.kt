4cafe1117bc2b9c4918e698fed7fb8cf6555c86b
Simon Fraser <github@sifraser.com>
2021-03-04 18:17:13 +0000

    Added css and top 10s

--- src/main/kotlin/com/sifraser/elevate/statsgen/ElevateStatsGen.kt
+++ src/main/kotlin/com/sifraser/elevate/statsgen/ElevateStatsGen.kt
@@ -2,7 +2,7 @@ package com.sifraser.elevate.statsgen
 
 import com.opencsv.CSVReaderHeaderAware
 import kotlinx.html.*
-import kotlinx.html.stream.createHTML
+import kotlinx.html.stream.appendHTML
 import java.io.File
 import java.io.FileReader
 import java.math.BigDecimal
@@ -23,6 +23,7 @@ class ElevateStatsGen(csv: File) {
             activities.add(Activity(
                     started = LocalDateTime.parse(line[0], ISO_OFFSET_DATE_TIME),
                     durationSec = parseDuration(line[3]),
+                    name = line[1],
                     distance = BigDecimal(line[4]),
                     elevationGain = BigDecimal(line[5]),
                     type = ActivityType.valueOf(line[2])
@@ -31,7 +32,7 @@ class ElevateStatsGen(csv: File) {
         activities
     }
 
-    private fun parseDuration(text: String) : BigDecimal {
+    private fun parseDuration(text: String): BigDecimal {
         val (h, m, s) = text.split(":").map { it.toInt() }
         val total = s + (m * 60) + (h * 60 * 60)
         return BigDecimal(total)
@@ -53,7 +54,8 @@ class ElevateStatsGen(csv: File) {
         }.toMap()
     }
 
-    private fun DIV.typeAndYearTable(getField: (Activity) -> BigDecimal) {
+    private fun BODY.typeAndYearTable(title: String, getField: (Activity) -> BigDecimal) {
+        h2 { +title }
         table {
             thead {
                 tr {
@@ -64,6 +66,7 @@ class ElevateStatsGen(csv: File) {
                     types.forEach {
                         th { +it.name }
                     }
+                    th { +"All" }
                 }
             }
             tbody {
@@ -71,43 +74,160 @@ class ElevateStatsGen(csv: File) {
                     tr {
                         th { +"$year" }
                         typeGroups.forEach { typeGroup ->
-                            val total = typeGroup.types.map { activitiesByTypeAndYear[it]?.get(year) ?: emptyList()}.flatten().sumBy(getField)
-                            td { +aggregationFormat.format(total) }
+                            val total = typeGroup.types.map {
+                                activitiesByTypeAndYear[it]?.get(year) ?: emptyList()
+                            }.flatten().sumBy(getField)
+                            td(classes = "number $typeGroup") { +aggregationFormat.format(total) }
                         }
                         types.forEach { type ->
                             val total = (activitiesByTypeAndYear[type]?.get(year) ?: emptyList()).sumBy(getField)
-                            td { +aggregationFormat.format(total) }
+                            td(classes = "number $type") { +aggregationFormat.format(total) }
                         }
+                        val yearTotal = (activitiesByYear[year] ?: emptyList()).sumBy(getField)
+                        td(classes = "number all") { +aggregationFormat.format(yearTotal) }
                     }
                 }
                 tr {
                     th { +"Total" }
                     typeGroups.forEach { typeGroup ->
-                        val total = typeGroup.types.map { activitiesByType[it] ?: emptyList()}.flatten().sumBy(getField)
-                        td { +aggregationFormat.format(total) }
+                        val total = typeGroup.types.map {
+                            activitiesByType[it] ?: emptyList()
+                        }.flatten().sumBy(getField)
+                        td(classes = "number total $typeGroup") { +aggregationFormat.format(total) }
                     }
                     types.forEach { type ->
                         val total = (activitiesByType[type] ?: emptyList()).sumBy(getField)
-                        td { +aggregationFormat.format(total) }
+                        td(classes = "number total $type") { +aggregationFormat.format(total) }
+                    }
+                    val total = activities.sumBy(getField)
+                    td(classes = "number total all") { +aggregationFormat.format(total) }
+                }
+            }
+        }
+    }
+
+    private fun BODY.top10Table(title: String, type: ActivityType, getField: (Activity) -> BigDecimal) =
+            top10Table(title, activitiesByType[type] ?: emptyList(), getField)
+
+    private fun BODY.top10Table(title: String, typeGroup: ActivityTypeGroup, getField: (Activity) -> BigDecimal) =
+            top10Table(title, typeGroup.types.map { activitiesByType[it] ?: emptyList() }.flatten(), getField)
+
+    private fun BODY.top10Table(title: String, act: List<Activity>, getField: (Activity) -> BigDecimal) {
+        h2 { +title }
+        top10Table(act.sortedByDescending(getField).take(10), getField)
+        act.groupBy { it.year }.toSortedMap().filter { (_, a) -> a.isNotEmpty() }.forEach { (y, a) ->
+            h3 { +"$y" }
+            top10Table(a.sortedByDescending(getField).take(10), getField)
+        }
+    }
+
+    private fun BODY.top10Table(top10: List<Activity>, getField: (Activity) -> BigDecimal) {
+        table {
+            thead {
+                tr {
+                    th { +"Measure" }
+                    th { +"Date" }
+                    th { +"Name" }
+                }
+            }
+            tbody {
+                top10.forEach {
+                    tr {
+                        td(classes = "number ${it.type}") { +singleFormat.format(getField(it)) }
+                        td(classes = "date ${it.type}") { +it.started.toLocalDate().toString() }
+                        td(classes = "name ${it.type}") { +it.name }
                     }
                 }
             }
         }
     }
 
+    private val css = """
+        html {
+            font-size: 12pt;
+            line-height: 1.2;
+        }
+        body {
+            font-family: georgia;
+            max-width: 1000px;
+            margin: 0 auto;
+        }
+	    table {
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+		th, td {
+            min-width: 80px;
+            padding: 5px;
+        }
+        td.number {
+			text-align: right;
+		}
+        td.Ride, td.VirtualRide, td.Cycling {
+            background: #B1D4EC;
+        }
+        td.Walk, td.Hike, td.Walking {
+            background: #F1D8C5;
+        }
+        td.Run {
+            background: #F9F0C2;
+        }
+        td.Rowing {
+            background: #B3B8DF;
+        }
+        td.Kayaking {
+            background: #ECE9DD;
+        }
+        td.all {
+            background: #D6F8E0;
+        }
+        td.total, td.all, th {
+            font-weight: bold;
+        }
+        th {
+            background: lightgray;
+        }
+    """
+
     fun writeStatsPage(output: File) {
-        output.writeText(createHTML().div {
-            h1 { +"Distance aggregates (km)" }
-            typeAndYearTable { it.distance }
-            h1 { +"Elevation gain aggregates (m)" }
-            typeAndYearTable { it.elevationGain }
-            h1 { +"Duration aggregates (min)" }
-            typeAndYearTable { it.durationMin }
-        })
+        output.writeText(StringBuilder().appendHTML().html {
+            head {
+                title { +"Activity stats" }
+                style { +css }
+            }
+            body {
+                h1 { +"Activity stats" }
+                typeAndYearTable("Distance aggregates (km)") { it.distance }
+                typeAndYearTable("Elevation gain aggregates (m)") { it.elevationGain }
+                typeAndYearTable("Duration aggregates (min)") { it.durationMin }
+
+                typeGroups.forEach {
+                    top10Table("Top 10 distance (km) - ${it.name}", it) { it.distance }
+                }
+                types.forEach {
+                    top10Table("Top 10 distance (km) - ${it.name}", it) { it.distance }
+                }
+                typeGroups.forEach {
+                    top10Table("Top 10 elevation gain (m) - ${it.name}", it) { it.elevationGain }
+                }
+                types.forEach {
+                    top10Table("Top 10 elevation gain (m) - ${it.name}", it) { it.elevationGain }
+                }
+                typeGroups.forEach {
+                    top10Table("Top 10 climb (m/km) - ${it.name}", it) { it.avgClimb }
+                }
+                types.forEach {
+                    top10Table("Top 10 climb (m/km) - ${it.name}", it) { it.avgClimb }
+                }
+            }
+        }.toString())
     }
 
     companion object {
         val aggregationFormat = DecimalFormat("#,###")
+        val singleFormat = DecimalFormat("###.0")
     }
 }
 
@@ -137,13 +257,14 @@ fun main(args: Array<String>) {
 data class Activity(
         val started: LocalDateTime,
         val type: ActivityType,
-//        val name: String,
+        val name: String,
         val durationSec: BigDecimal,
         val distance: BigDecimal,
         val elevationGain: BigDecimal
 ) {
     val year by lazy { started.year }
     val durationMin by lazy { durationSec.divide(BigDecimal(60), 0, RoundingMode.UP) }
+    val avgClimb by lazy { if (distance == BigDecimal.ZERO) BigDecimal.ZERO else elevationGain.divide(distance, 2, RoundingMode.UP) }
 }
 
 enum class ActivityType {
